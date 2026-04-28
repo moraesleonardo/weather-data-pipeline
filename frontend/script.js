95ae7cb3c377bc5a840484e4d72a344806dfe6fa
Leonardo Moraes <leonardo.moraes1@yahoo.com>
2026-04-28 13:29:12 -0300

    Configure frontend API URL for VPS deployment

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:5000";
+const API_BASE_URL = "http://31.97.170.27:5000";
 
 const regionSelect = document.getElementById("regionSelect");
 const variableSelect = document.getElementById("variableSelect");
